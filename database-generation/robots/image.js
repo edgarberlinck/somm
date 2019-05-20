c3f84d1095beb8c51e4bd0b0c98c7ac9daa341ba
Edgar Muniz Berlinck <edgar.vv@gmail.com>
2019-05-20 13:41:39 -0300

    FInal version of Image Downloader Robot

--- database-generation/robots/image.js
+++ database-generation/robots/image.js
@@ -1,36 +1,29 @@
 const imageDownloader = require('image-downloader')
 const google = require('googleapis').google
 const customSearch = google.customsearch('v1')
-//const state = require('./state.js')
+const state = require('./state.js')
 var shell = require('shelljs');
 
 const googleSearchCredentials = require('../credentials/google-search.json')
 
 async function robot() {
     console.log('> [image-robot] Starting...')
-    //const content = state.load()
-    const content = {
-        length: 1,
-        data: [{
-            wine: 'domaine de la romanée-conti romanée-conti grand cru',
-            vintage: 2011
-        }]
-    }
-
+    const content = state.load()
     await fetchImagesOfAllSentences(content)
     await downloadAllImages(content)
-
-    //state.save(content)
+    
+    state.save(content)
 
     async function fetchImagesOfAllSentences(content) {
         for (let sentenceIndex = 0; sentenceIndex < content.length; sentenceIndex++) {
             let query
 
-            query = `${content.data[sentenceIndex].wine} ${content.data[sentenceIndex].vintage}`
+            query = `Label of ${content.data[sentenceIndex].wine} ${content.data[sentenceIndex].vintage} ${content.data[sentenceIndex].color}`
+            queryByLabel = `label-of-${content.data[sentenceIndex].label}-${content.data[sentenceIndex].vintage}-${content.data[sentenceIndex].color}`
             
-            console.log(`> [image-robot] Querying Google Images with: "${query}"`)
+            console.log(`> [image-robot] Querying Google Images with: "${query}" and "${queryByLabel}"`)
 
-            content.data[sentenceIndex].images = await fetchGoogleAndReturnImagesLinks(query)
+            content.data[sentenceIndex].images = [...await fetchGoogleAndReturnImagesLinks(query), ...await fetchGoogleAndReturnImagesLinks(queryByLabel)]
             content.data[sentenceIndex].googleSearchQuery = query
         }
     }
@@ -56,7 +49,7 @@ async function robot() {
 
         for (let sentenceIndex = 0; sentenceIndex < content.length; sentenceIndex++) {
             const images = content.data[sentenceIndex].images
-
+            
             for (let imageIndex = 0; imageIndex < images.length; imageIndex++) {
                 const imageUrl = images[imageIndex]
 
@@ -64,21 +57,24 @@ async function robot() {
                     if (content.downloadedImages.includes(imageUrl)) {
                         throw new Error('Image already downloaded')
                     }
-                    const folderName = `${content.data[sentenceIndex].wine}-${content.data[sentenceIndex].vintage}`;    
+                    const folderName = `${content.data[sentenceIndex].label}-${content.data[sentenceIndex].color}-${content.data[sentenceIndex].vintage}`;    
                     
-                    createDatasetDirectory(folderName);
-                    await downloadAndSave(imageUrl, folderName, `${sentenceIndex}-original.png`)
+                    await createDatasetDirectory(folderName);
+                    await downloadAndSave(imageUrl, folderName, `${sentenceIndex}-${imageIndex}-original.png`)
                     content.downloadedImages.push(imageUrl)
                     console.log(`> [image-robot] [${sentenceIndex}][${imageIndex}] Image successfully downloaded: ${imageUrl}`)
-                    break
+                    continue
                 } catch (error) {
                     console.log(`> [image-robot] [${sentenceIndex}][${imageIndex}] Error (${imageUrl}): ${error}`)
                 }
             }
         }
     }
-    function createDatasetDirectory(folderName) {
-        shell.mkdir('-p', `./content/${folderName}/`);
+    async function createDatasetDirectory(folderName) {
+        return new Promise(resolve => {
+            shell.mkdir('-p', `./content/${folderName}/`);
+            resolve(true);
+        })
     }
     async function downloadAndSave(url, folderName, fileName) {
         return imageDownloader.image({
